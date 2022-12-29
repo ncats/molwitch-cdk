a116940ab45b6b1b28acb35f54a4b6e93dec86f7
Mitch Miller <mitch.miller@thinkscience.us>
2022-12-29 15:30:58 -0500

    updated copyright year to 2023

--- src/test/java/TestParseMolWithSgroupsAndClone.java
+++ src/test/java/TestParseMolWithSgroupsAndClone.java
@@ -1,7 +1,7 @@
 /*
  * NCATS-MOLWITCH-CDK
  *
- * Copyright (c) 2020.
+ * Copyright (c) 2023.
  *
  * This work is free software; you can redistribute it and/or modify it under the terms of the
  * GNU Lesser General Public License as published by the Free Software Foundation;
